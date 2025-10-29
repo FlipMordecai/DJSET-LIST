6291918e497d20497395b42615e7130f0655436c
FlipMordecai <52514180+FlipMordecai@users.noreply.github.com>
2025-10-28 21:07:13 -0600

    Update store.php

--- store.php
+++ store.php
@@ -1,7 +1,7 @@
 <?php
 
 $myfile = fopen("location.txt", "w")
-$txt = "Lat: " . $_GET["lat"] . "";
+$txt = "Lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"];
 fwrite($myfile, $txt);
 fclose($myfile);
   
